c6e65d28e88433f8bb3195ea4a7af9cc457bfbb7
전병길 <jeonbg@kakao.com>
2024-10-30 01:17:05 +0900

    feat: 회원가입 서버액션 내 Zod 추가

--- app/(auth)/create-account/page.tsx
+++ app/(auth)/create-account/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import SocialLoginButton from "@/components/button/social-login-button";
 import FormButton from "@/components/form/form-button";
 import FormInput from "@/components/form/form-input";
 
+import { createAccount } from "./actions";
+import { useActionState } from "react";
+
 export default function CreateAccount() {
+  const [state, formAction] = useActionState(createAccount, null);
+
+  console.log(state);
+
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -11,28 +20,37 @@ export default function CreateAccount() {
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
 
-      <form className="flex flex-col gap-3">
+      <form action={formAction} className="flex flex-col gap-3">
         <FormInput
           name="username"
           type="text"
           placeholder="Username"
           required
+          errors={state?.fieldErrors?.username}
         />
 
-        <FormInput name="email" type="email" placeholder="Email" required />
+        <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={state?.fieldErrors?.email}
+        />
 
         <FormInput
           name="password"
           type="password"
           placeholder="Password"
           required
+          errors={state?.fieldErrors?.password}
         />
 
         <FormInput
-          name="confirmPassword"
+          name="passwordConfirm"
           type="password"
           placeholder="Confirm password"
           required
+          errors={state?.fieldErrors?.passwordConfirm}
         />
 
         <FormButton label="Create account" />
